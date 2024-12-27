ae0e4954365891c1279235a51fc6bb0c5671de49
nathanchu <nathan.ch.chu@gmail.com>
2024-12-27 01:30:04 -0500

    use json parse

--- backend/api/server.js
+++ backend/api/server.js
@@ -15,19 +15,7 @@ const nodemailer = require('nodemailer'); // Import nodemailer
 const admin = require('firebase-admin'); // Import Firebase Admin SDK
 const config = require('../src/config'); // Import config
 
-const serviceAccount = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: "googleapis.com"
-}
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 console.log("service account: ", serviceAccount);
 
 admin.initializeApp({
